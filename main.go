bc441cc49da93a6287780a73913d425ba0add427
litxzo <1044005014@qq.com>
2023-03-30 20:28:02 +0800

    封装了日志

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
-	// "gin_project/router"
-	"fmt"
+	"gin_project/bootstrap"
+	"gin_project/global"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// router := router.WebStart()
-	// router.Run()
-	fmt.Println("hello")
+	// 初始化配置
+	bootstrap.InitializeConfig()
+
+	//初始化日志
+	global.App.Log = bootstrap.InitializeLog()
+	global.App.Log.Info("log init success!")
+
+	r := gin.Default()
+
+	// 测试路由
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	// 启动服务器
+	r.Run(":" + global.App.Config.App.Port)
 }
